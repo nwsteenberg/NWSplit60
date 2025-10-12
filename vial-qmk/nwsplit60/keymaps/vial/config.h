8ef308c65895b71b843ed36898f0250e73847d2e
Nikolaj Wolder Steenberg <nikolajws@protonmail.com>
2025-10-12 20:34:30 +0200

    Added Vial/QMK firmware

---
+++ vial-qmk/nwsplit60/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1D, 0x62, 0x00, 0x2A, 0x26, 0x54, 0x1C, 0x21}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 4, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 7, 7 }
+
